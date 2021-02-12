460bb265743635dd78d9697177eef3ca097dc110
Deepan Kalyanasundaram <dkalyan1@in.ibm.com>
2021-02-12 11:23:54 +0530

    payment details page

--- client/src/app/layout/payment/view-payment/view-payment.component.ts
+++ client/src/app/layout/payment/view-payment/view-payment.component.ts
@@ -4,6 +4,8 @@ import { CommonService } from 'src/app/services/common.service';
 import { DatePipe } from '@angular/common';
 import { environment } from 'src/environments/environment';
 import { Payment } from 'src/app/interfaces/payments';
+import { BootstrapService } from 'src/app/services/bootstrap.service';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-view-payment',
@@ -12,9 +14,10 @@ import { Payment } from 'src/app/interfaces/payments';
 })
 export class ViewPaymentComponent implements OnInit {
   enableSearch:boolean = false;
-  displayedColumns = ['type','amount','actions'];
+  displayedColumns = ['customer','type','amount','createdBy','actions'];
   dataSource: MatTableDataSource<Payment>;
   payments: Payment[];
+  tempPayment: Payment[];
   dedicatedCustomer:boolean=false;
   maxToDate: Date = new Date();
   pDate: Date = new Date();
@@ -22,29 +25,75 @@ export class ViewPaymentComponent implements OnInit {
   confirmBox: string = "YES";
   delDate: Date;
   selRoute: string = "all";
+  selUser: string = "all";
   routes:any;
+  usersList:any;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   
-  constructor(private dialog: MatDialog,public commonService: CommonService, public snackBar:MatSnackBar,
+  constructor(private dialog: MatDialog,private bootstrap:BootstrapService,public commonService: CommonService, public snackBar:MatSnackBar,
     private datePipe: DatePipe,@Inject(MAT_DIALOG_DATA) public form_value: any) { }
 
   ngOnInit() {
+    this.loadMasters();
     this.loadPayments(); 
+  }
+
+  loadMasters(){
+    this.routes = this.bootstrap.routes.map(function(val) {
+      return {
+        key:val._id,
+        value:val.areaName
+      }
+    });
+    this.routes.push({key:'all',value:'All'});
+
+    this.commonService.getMethod(environment.urls.getUser).subscribe((data:any)=> {
+      this.usersList = data.map(function(val) {
+        return {
+          key:val._id,
+          value:val.username
+        }
+      });
+      this.usersList.push({key:'all',value:'All'});
+    });    
   }
 
   addEvent(){
     console.log('test');
   }
 
+  getTotalCost(){
+    return (this.tempPayment.map(t => t.amount).reduce((acc, value) => acc + value, 0));
+  }
+
+  filterOpts(){
+    //console.log(this.selUser);
+    this.tempPayment = this.payments;
+    if(this.selUser != 'all'){
+      this.tempPayment = this.tempPayment.filter(val => val.createdBy == this.selUser);
+    } 
+
+    if(this.selRoute !='all'){
+      this.tempPayment = this.tempPayment.filter(val => val.customer[0].route == this.selRoute);
+    }
+    
+    this.loadTable(this.tempPayment);
+  }
+
   loadPayments(){
-    var url = environment.urls.getPayment+'?pdate='+this.datePipe.transform(this.pDate,"dd-MM-yyyy");
+    var url = environment.urls.getPayment+'?pdate='+this.datePipe.transform(this.delDate,"dd-MM-yyyy");
     if(this.dedicatedCustomer){
       //url+="&cust_id="+this.currentCustomer._id;
     }
     this.commonService.getMethod(url).subscribe((data:Payment[]) => {
-      this.payments = data;
-      this.dataSource = new MatTableDataSource(this.payments);
+      this.payments = data;    
+      this.filterOpts();
+    });
+  }
+
+  loadTable(payments:any){
+    this.dataSource = new MatTableDataSource(payments);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;      
       // this.dataSource.filterPredicate = (data, filter: string) => {
@@ -59,7 +108,6 @@ export class ViewPaymentComponent implements OnInit {
         const transformedFilter = filter.trim().toLowerCase();
         return dataStr.indexOf(transformedFilter) !== -1;
       };
-    });
   }
 
   applyFilter(filterValue: string) {
