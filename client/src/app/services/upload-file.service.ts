719d1787c0e24fdd42d8c0fc5a9c551238bcadd6
dk <deepan.k@blackboard.com>
2022-10-30 19:12:36 +0530

    additional file for file upload service

---
+++ client/src/app/services/upload-file.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest, HttpHeaders, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadFileService {
+  private uploadUrl = environment.urls.prodBulkUpload;
+  constructor(private http: HttpClient) { }
+  upload(file: File): Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+    formData.append('file', file);
+    const req = new HttpRequest('POST', `${this.uploadUrl}`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+    return this.http.request(req);
+  }
+  getFiles(): Observable<any> {
+    return this.http.get(`${this.uploadUrl}/files`);
+  }
+}
