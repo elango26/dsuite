ded097fffd46dda49c6aa9394a8299a0579df6cc
dk <deepan.k@blackboard.com>
2023-04-22 23:36:54 +0530

    Fyear and parent date in transactionDetails

--- routes/manualScripts.js
+++ routes/manualScripts.js
@@ -2,6 +2,191 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const common = require('./common');
+// const transactionDetails = require('../models/transactiondetails');
+const {ObjectId} = require('mongodb');
+
+router.get('/updateAllTransactionsWithDateOrder',(req,res,next)=>{ 
+    // const modelNames = new Map([["Sales","sale_date"],["Orders","order_date"],["Damage","damage_date"],["Purchase","purchase_date"]]);
+    const mySchema = mongoose.model("transactionDetails");
+    mySchema.aggregate([
+        {$match:{
+            type: 'ORDER',
+            // parent_id: ObjectId('600716b4dea12309085305fd')
+        }},
+        {$lookup:{
+            from: "orders",
+            localField: "parent_id",
+            foreignField: "_id",
+            as: "order"
+        }},
+        {$unwind:{
+            path: '$order',
+            // includeArrayIndex: 'string',
+            preserveNullAndEmptyArrays: true
+        }},
+        {$set:{
+            parent_date: "$order.order_date"
+        }},
+        {$project:{
+            order: 0
+        }},
+    ]).exec((err,doc)=>{
+        if(err) res.json(err);
+        console.log(doc.length);
+        const updateTransObj = [];
+        doc.forEach((item) => {
+            updateTransObj.push({
+                updateOne: {
+                    filter: { _id: item._id },
+                    update: { $set: item }
+                  }
+            });
+            // mySchema.updateOne({_id: item._id}, {$set: item});
+        });
+        mySchema.bulkWrite(updateTransObj,{ordered:false}).then(sucess=>{
+            res.json("order done");
+        }).catch(error=>{
+            res.json(error);
+        }); 
+    })
+});
+
+router.get('/updateAllTransactionsWithDateSales',(req,res,next)=>{ 
+    // const modelNames = new Map([["Sales","sale_date"],["Orders","order_date"],["Damage","damage_date"],["Purchase","purchase_date"]]);
+    const mySchema = mongoose.model("transactionDetails");
+    mySchema.aggregate([
+        {$match:{
+            type: 'SALES',
+            // parent_id: ObjectId('600716b4dea12309085305fd')
+        }},
+        {$lookup:{
+            from: "sales",
+            localField: "parent_id",
+            foreignField: "_id",
+            as: "sales"
+        }},
+        {$unwind:{
+            path: '$sales',
+            // includeArrayIndex: 'string',
+            preserveNullAndEmptyArrays: true
+        }},
+        {$set:{
+            parent_date: "$sales.sale_date"
+        }},
+        {$project:{
+            sales: 0
+        }},
+    ]).exec((err,doc)=>{
+        if(err) res.json(err);
+        const updateTransObj = [];
+        doc.forEach((item) => {
+            updateTransObj.push({
+                updateOne: {
+                    filter: { _id: item._id },
+                    update: { $set: item }
+                  }
+            });
+            // mySchema.updateOne({_id: item._id}, {$set: item});
+        });
+        mySchema.bulkWrite(updateTransObj,{ordered:false}).then(sucess=>{
+            res.json("sales done");
+        }).catch(error=>{
+            res.json(error);
+        }); 
+    })
+});
+
+router.get('/updateAllTransactionsWithDatePurchase',(req,res,next)=>{ 
+    // const modelNames = new Map([["Sales","sale_date"],["Orders","order_date"],["Damage","damage_date"],["Purchase","purchase_date"]]);
+    const mySchema = mongoose.model("transactionDetails");
+    mySchema.aggregate([
+        {$match:{
+            type: 'PURCHASE',
+            // parent_id: ObjectId('600716b4dea12309085305fd')
+        }},
+        {$lookup:{
+            from: "purchases",
+            localField: "parent_id",
+            foreignField: "_id",
+            as: "purchase"
+        }},
+        {$unwind:{
+            path: '$purchase',
+            // includeArrayIndex: 'string',
+            preserveNullAndEmptyArrays: true
+        }},
+        {$set:{
+            parent_date: "$purchase.purchase_date"
+        }},
+        {$project:{
+            purchase: 0
+        }},
+    ]).exec((err,doc)=>{
+        if(err) res.json(err);
+        console.log(doc.length);
+        const updateTransObj = [];  
+        doc.forEach((item) => {
+            updateTransObj.push({
+                updateOne: {
+                    filter: { _id: item._id },
+                    update: { $set: item }
+                  }
+            });
+            // mySchema.updateOne({_id: item._id}, {$set: item});
+        });
+        console.log(updateTransObj);
+        mySchema.bulkWrite(updateTransObj,{ordered:false}).then(sucess=>{
+            res.json("purchase done");
+        }).catch(error=>{
+            res.json(error);
+        }); 
+    })
+});
+
+router.get('/updateAllTransactionsWithDateDamage',(req,res,next)=>{ 
+    // const modelNames = new Map([["Sales","sale_date"],["Orders","order_date"],["Damage","damage_date"],["Purchase","purchase_date"]]);
+    const mySchema = mongoose.model("transactionDetails");
+    mySchema.aggregate([
+        {$match:{
+            type: 'DAMAGE',
+            // parent_id: ObjectId('600716b4dea12309085305fd')
+        }},
+        {$lookup:{
+            from: "damages",
+            localField: "parent_id",
+            foreignField: "_id",
+            as: "damage"
+        }},
+        {$unwind:{
+            path: '$damage',
+            // includeArrayIndex: 'string',
+            preserveNullAndEmptyArrays: true
+        }},
+        {$set:{
+            parent_date: "$damage.damage_date"
+        }},
+        {$project:{
+            damage: 0
+        }},
+    ]).exec((err,doc)=>{
+        if(err) res.json(err);
+        const updateTransObj = [];
+        doc.forEach((item) => {
+            updateTransObj.push({
+                updateOne: {
+                    filter: { _id: item._id },
+                    update: { $set: item }
+                  }
+            });
+            // mySchema.updateOne({_id: item._id}, {$set: item});
+        });
+        mySchema.bulkWrite(updateTransObj,{ordered:false}).then(sucess=>{
+            res.json("damage done");
+        }).catch(error=>{
+            res.json(error);
+        }); 
+    })
+});
 
 router.get('/updateAllCollectionsWithFYear',(req,res,next)=>{
     const modelNames = mongoose.modelNames();
@@ -22,6 +207,7 @@ router.get('/updateAllCollectionsWithFYear',(req,res,next)=>{
             endDate.setMonth(2);
             endDate.setUTCFullYear(year);
             console.log("start and end date", startDate +"--"+ endDate);
+            // add query contraints to check for ist times
             const query = { createdAt: { $gte: startDate, $lte: endDate } };
 
             // Update the matched documents
